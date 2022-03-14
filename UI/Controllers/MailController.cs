2222881be9aa6782ab62a4653abf1c8213c31a79
mohammedsallam <mohammedsallam812@gmail.com>
2022-03-14 16:02:19 +0200

    ForgetPassword-ResetPassword

---
+++ UI/Controllers/MailController.cs
@@ -0,0 +1,26 @@
+﻿using BLL.Helper;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Controllers
+{
+    public class MailController : Controller
+    {
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(MailViewModel model)
+        {
+            TempData["Msg"] = MailSender.SendMail(model);
+            ModelState.Clear();
+            return View();
+        }
+    }
+}
