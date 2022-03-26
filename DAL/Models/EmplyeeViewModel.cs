2e9e31879e7bd98678cd267b009fb36dbd117fcd
mohammedsallam <mohammedsallam812@gmail.com>
2022-03-26 18:47:48 +0200

    Sallam

--- DAL/Models/EmplyeeViewModel.cs
+++ DAL/Models/EmplyeeViewModel.cs
@@ -13,18 +13,24 @@ namespace DAL.Models
     public class EmplyeeViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is Required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "SSN is Required")]
         public string SSN { get; set; }
+        [Required(ErrorMessage = "BirthDate is Required")]
         public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "Phone is Required")]
         public string Phone { get; set; }
         public string AnotherPhone { get; set; }
         public string Gender { get; set; }
+        [Required(ErrorMessage = "Address is Required")]
         public string Address { get; set; }
         public DateTime WorkStartTime { get; set; }
         public string Photo { get; set; }
         public bool IsActive { get; set; }
         public int? ShiftId { get; set; }
         public string UserId { get; set; }
+
         [Required(ErrorMessage = "Email Required")]
         [EmailAddress(ErrorMessage = "You Must Enter Valid Email")]
         public string Email { get; set; }
