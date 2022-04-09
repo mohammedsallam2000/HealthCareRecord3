bf04fe524c2190ed43d0485d7856e627bf01d0c3
ELsobky20 <ahmedelsobky630@gmail.com>
2022-04-09 18:48:15 +0200

    DoctorWork

--- UI/wwwroot/receptionist dashbord/dist/js/add-more.js
+++ UI/wwwroot/receptionist dashbord/dist/js/add-more.js
@@ -1,55 +1,114 @@
-function add(id) {
-    let iconClass='';
-    let iconClassRoom='';
-    let newBox = document.createElement("div");
-    console.log(id);
-    if(id==='analyzes')
-      iconClass= "fas fa-clipboard-list";
-    else if(id==='radiology')
-      iconClass ='fas fa-x-ray'
-    else if(id==='Medicine')
-      iconClass ='fas fa-capsules'
-    else if(id==='room')
-      iconClass ='fas fa-hospital'
-    else if(id==='consultations')
-      iconClass ='fas fa-x-ray'
-    else
-      iconClass ='fas fa-medkit';
-  
-      if(id==='room')
-        iconClassRoom ='fas fa-procedures';
-      else
-        iconClassRoom='fas fa-money-bill-alt'
-  
-    newBox.classList.add("row");
+function add(id, data) {
+  let x = '';
+  let newBox = document.createElement("div");
+  console.log(id);
+  if (id === 'analyzes') {
+    x = 'name Lab';
+    iconClass = "fas fa-clipboard-list";
+
+  }
+  else if (id === 'radiology') {
+    x = 'name radiology';
+    iconClass = 'fas fa-x-ray'
+  }
+  else if (id === 'Medicine') {
+    x = 'name Medicine';
+    iconClass = 'fas fa-capsules'
+  }
+  else if (id === 'room') {
+    x = 'name reservation';
+    iconClass = 'fas fa-hospital'
+  }
+  else if (id === 'consultations') {
+
+    iconClass = 'fas fa-x-ray'
+  }
+  else
+    iconClass = 'fas fa-medkit';
+
+  if (id === 'room')
+    iconClassRoom = 'fas fa-procedures';
+  else
+    iconClassRoom = 'fas fa-money-bill-alt'
+
+  newBox.classList.add("row");
+
+  if (id != 'room') {
     newBox.innerHTML = `
     <!--${id}  -->
-      <div class="col-md-6 col-sm-12 mb-2">
-      <div class="form-floating input-group">
-        <span class="input-group-text" for="Name">
-            <i class="${iconClass}"></i>
-        </span>
-        <input class="form-control form-control show-tick"
-            list="${id}" id="exampleDataList"
-            placeholder="${id}">      
-        <label for="floatingInput">- ${id} -</label>
-      </div>
-      </div>
-      <!-- price   -->
-      <div class="col-11 col-sm-11 col-md-5 mb-2">
-      <div class="form-floating input-group">
-        <span class="input-group-text">
-            <i class="${iconClassRoom}"></i>
-        </span>
-        <p class="form-control ">630</p>
-      </div>
-      </div>
-      <div class="col-1 col-sm-1 col-md-1 mb-2">
-      <button type="button"
-        class="btn btn-primary mt-2 more " onclick="add('${id}')">+</button>
-      </div>
+   <div class="col-md-6 col-sm-12 mb-2">
+                                                        <div class="form-floating input-group">
+                                                            <span class="input-group-text" for="Name">
+                                                                <i class="fas fa-capsules"></i>
+                                                            </span>
+                                                            <input class="form-control form-control show-tick"
+                                                                list="${x}" id="lab"
+                                                                placeholder="Lab">
+                                                            <datalist   id="${x}">
+                                                                <option value="lab"/>
+                                                                @foreach (var item in ${data})
+                                                               {
+                                                                    <option value="@item.Name"/>
+                                                               }
+
+                                                            </datalist>
+                                                            <label for="floatingInput">- ${id} -</label>
+                                                        </div>
+                                                    </div>
+
+
+                                                    <div class="col-1 col-sm-1 col-md-1 mb-2">
+                                                        <button type="button" class="btn btn-primary mt-2 more"
+                                                            onclick="add('${id}','${data}')">+</button>
+                                                    </div>
   `;
-  
-    document.getElementById(`${id}`).append(newBox);
   }
-  
+  else if (id == 'room') {
+    newBox.innerHTML = `
+    <!--${id}  -->
+   <div class="col-md-6 col-sm-12 mb-2">
+                                                        <div class="form-floating input-group">
+                                                            <span class="input-group-text" for="Name">
+                                                                <i class="fas fa-capsules"></i>
+                                                            </span>
+                                                            <input class="form-control form-control show-tick"
+                                                                list="name reservation" id="lab"
+                                                                placeholder="Lab">
+                                                            <datalist   id="name reservation">
+                                                                <option value="- floor -"/>
+                                                                @foreach (var item in ${data})
+                                                               {
+                                                                    <option value="@item.Floor"/>
+                                                               }
+
+                                                            </datalist>
+                                                            <label for="floatingInput">- floor -</label>
+                                                        </div>
+                                                    </div>
+ <div class="col-11 col-sm-11 col-md-5 mb-2">
+                                    <div class="form-floating input-group">
+                                        <span class="input-group-text" for="Name">
+                                            <i class="fas fa-procedures"></i>
+                                        </span>
+                                        <input class="form-control form-control show-tick"
+                                               list="name reservation"
+                                               placeholder="name reservation">
+                                        <datalist id="name reservation">
+                                            <option value="- room -"/>
+                                            @foreach (var item in ${data})
+                                            {
+                                                <option value="@item.Number"/>
+                                            }
+                                        </datalist>
+                                        <label for="floatingInput">- room -</label>
+                                    </div>
+                                </div>
+
+                                                    <div class="col-1 col-sm-1 col-md-1 mb-2">
+                                                        <button type="button" class="btn btn-primary mt-2 more"
+                                                            onclick="add('${id}','${data}')">+</button>
+                                                    </div>
+  `;
+  }
+  document.getElementById(`${id}`).append(newBox);
+}
