927ea9d10cb077e62e8954089ff55a5a0d237162
mohammedsallam <mohammedsallam812@gmail.com>
2022-04-10 20:43:16 +0200

    Sallam

--- DAL/Migrations/20220410184228_AddPriceColmnAtSurgeryEntity.Designer.cs
+++ DAL/Migrations/20220410184228_AddPriceColmnAtSurgeryEntity.Designer.cs
@@ -4,14 +4,16 @@ using DAL.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(AplicationDbContext))]
-    partial class AplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220410184228_AddPriceColmnAtSurgeryEntity")]
+    partial class AddPriceColmnAtSurgeryEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
