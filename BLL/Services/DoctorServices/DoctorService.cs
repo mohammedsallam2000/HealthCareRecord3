88093b3ca33165f707d45a0e38bba7ed232e899d
mohammedsallam <mohammedsallam812@gmail.com>
2022-05-07 03:19:19 +0200

    Sallam

--- BLL/Services/DoctorServices/DoctorService.cs
+++ BLL/Services/DoctorServices/DoctorService.cs
@@ -16,38 +16,41 @@ namespace BLL.Services
         private readonly AplicationDbContext context;
         private UserManager<IdentityUser> userManager;
         private readonly RoleManager<IdentityRole> roleManager;
-        public DoctorService(AplicationDbContext context , UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        public DoctorService(AplicationDbContext context, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             this.userManager = userManager;
             this.roleManager = roleManager;
             this.context = context;
-        } 
+        }
         public async Task<int> Add(DoctorViewModel doc)
         {
 
-                Doctor obj = new Doctor();
-                obj.Name = doc.Name;
-                obj.SSN = doc.SSN;
-                obj.Phone = doc.Phone;
-                obj.Address = doc.Address;
-                obj.ShiftId = doc.ShiftId;
-                obj.BirthDate = doc.BirthDate;
-                obj.Degree = doc.Degree;            
-                obj.Gender = doc.Gender;
+            Doctor obj = new Doctor();
+            obj.Name = doc.Name;
+            obj.SSN = doc.SSN;
+            obj.Phone = doc.Phone;
+            obj.Address = doc.Address;
+            obj.ShiftId = doc.ShiftId;
+            obj.BirthDate = doc.BirthDate;
+            obj.Degree = doc.Degree;
+            obj.Gender = doc.Gender;
+            obj.Facebook = doc.Facebook;
+            obj.Whatsapp = doc.Whatsapp;
+            obj.Twitter = doc.Twitter;
             obj.IsActive = false;
 
             obj.DepartmentId = doc.DepartmentId;
-                obj.Photo = UploadFileHelper.SaveFile(doc.PhotoUrl, "Photos");
-                var user = new IdentityUser()
-                {
-                    Email = doc.Email,
-                    UserName = doc.Email,
-                };
-                var result = await userManager.CreateAsync(user, doc.Password);
-                var user2 = await userManager.FindByEmailAsync(doc.Email);
+            obj.Photo = UploadFileHelper.SaveFile(doc.PhotoUrl, "Photos");
+            var user = new IdentityUser()
+            {
+                Email = doc.Email,
+                UserName = doc.Email,
+            };
+            var result = await userManager.CreateAsync(user, doc.Password);
+            var user2 = await userManager.FindByEmailAsync(doc.Email);
             var DepartmentName = context.Departments.Where(x => x.DepartmentId == doc.DepartmentId).Select(x => x.Name).FirstOrDefault();
-                if (DepartmentName == "Analysis")
-                {
+            if (DepartmentName == "Analysis")
+            {
                 //Create Role LabDoctor if not found
                 var TestRole = await roleManager.RoleExistsAsync("AnalysisDoctor");
                 if (!TestRole)
@@ -57,9 +60,9 @@ namespace BLL.Services
                 }
                 // put LabDoctor in LabDoctor role
                 var result2 = await userManager.AddToRoleAsync(user2, "AnalysisDoctor");
-                }
-                else if(DepartmentName == "Radiology")
-                {
+            }
+            else if (DepartmentName == "Radiology")
+            {
                 //Create Role RadiologyDoctor if not found
                 var TestRole = await roleManager.RoleExistsAsync("RadiologyDoctor");
                 if (!TestRole)
@@ -69,67 +72,70 @@ namespace BLL.Services
                 }
                 // put RadiologyDoctor in RadiologyDoctor role
                 var result2 = await userManager.AddToRoleAsync(user2, "RadiologyDoctor");
-                }
-                else if (DepartmentName == "Pharmacy")
+            }
+            else if (DepartmentName == "Pharmacy")
+            {
+                //Create Role Pharmacist if not found
+                var TestRole = await roleManager.RoleExistsAsync("Pharmacist");
+                if (!TestRole)
                 {
-                    //Create Role Pharmacist if not found
-                    var TestRole = await roleManager.RoleExistsAsync("Pharmacist");
-                    if (!TestRole)
-                    {
-                        var role = new IdentityRole { Name = "Pharmacist" };
-                        await roleManager.CreateAsync(role);
-                    }
-                    // put Pharmacist in Pharmacist role
-                    var result2 = await userManager.AddToRoleAsync(user2, "Pharmacist");
+                    var role = new IdentityRole { Name = "Pharmacist" };
+                    await roleManager.CreateAsync(role);
                 }
-                else
+                // put Pharmacist in Pharmacist role
+                var result2 = await userManager.AddToRoleAsync(user2, "Pharmacist");
+            }
+            else
+            {
+                //Create Role Doctor if not found
+                var TestRole = await roleManager.RoleExistsAsync("Doctor");
+                if (!TestRole)
                 {
-                    //Create Role Doctor if not found
-                    var TestRole = await roleManager.RoleExistsAsync("Doctor");
-                    if (!TestRole)
-                    {
-                        var role = new IdentityRole { Name = "Doctor" };
-                        await roleManager.CreateAsync(role);
-                    }
-                    // put Doctor in Doctor role
-                    var result2 = await userManager.AddToRoleAsync(user2, "Doctor");
+                    var role = new IdentityRole { Name = "Doctor" };
+                    await roleManager.CreateAsync(role);
                 }
-                if (result.Succeeded)
+                // put Doctor in Doctor role
+                var result2 = await userManager.AddToRoleAsync(user2, "Doctor");
+            }
+            if (result.Succeeded)
+            {
+                obj.UserId = user2.Id;
+                await context.Doctors.AddAsync(obj);
+                int res = await context.SaveChangesAsync();
+                if (res > 0)
                 {
-                    obj.UserId = user2.Id;
-                    await context.Doctors.AddAsync(obj);
-                    int res = await context.SaveChangesAsync();
-                    if (res > 0)
-                    {
-                        return obj.Id;
-                    }
-                    return 0;
+                    return obj.Id;
                 }
                 return 0;
+            }
+            return 0;
         }
         public async Task<int> Update(DoctorViewModel doc)
         {
             var OldData = context.Doctors.FirstOrDefault(x => x.Id == doc.Id);
-                OldData.Name = doc.Name;
-                OldData.BirthDate = doc.BirthDate;
+            OldData.Name = doc.Name;
+            OldData.BirthDate = doc.BirthDate;
             //OldData.Gender = doc.Gender;
             OldData.ShiftId = doc.ShiftId;
             OldData.Address = doc.Address;
+            OldData.Facebook = doc.Facebook;
+            OldData.Whatsapp = doc.Whatsapp;
+            OldData.Twitter = doc.Twitter;
             //OldData.Phone = doc.Phone;
             //OldData.Photo = UploadFileHelper.SaveFile(doc.PhotoUrl, "Photos");
             var user = await userManager.FindByIdAsync(OldData.UserId);
-                user.Email = doc.Email;
-                user.UserName = doc.Email;
-                var result = await userManager.UpdateAsync(user);
-                await context.SaveChangesAsync();
-                return 0;
-           
+            user.Email = doc.Email;
+            user.UserName = doc.Email;
+            var result = await userManager.UpdateAsync(user);
+            await context.SaveChangesAsync();
+            return 0;
+
         }
 
 
 
 
-        public async Task <bool> Delete(int id)
+        public async Task<bool> Delete(int id)
         {
             try
             {
@@ -149,13 +155,13 @@ namespace BLL.Services
                 return false;
             }
         }
-    
-       
+
+
 
         public IEnumerable<DoctorViewModel> GetAll()
         {
             List<DoctorViewModel> doc = new List<DoctorViewModel>();
-            foreach (var item in context.Doctors.Where(x=>x.IsActive==false))
+            foreach (var item in context.Doctors.Where(x => x.IsActive == false))
             {
                 DoctorViewModel obj = new DoctorViewModel();
                 obj.Address = item.Address;
@@ -174,26 +180,26 @@ namespace BLL.Services
             return doc;
         }
 
-        public async  Task<DoctorViewModel> GetByID(int id)
+        public async Task<DoctorViewModel> GetByID(int id)
         {
             var user = await userManager.FindByIdAsync(context.Doctors.Where(x => x.Id == id).Select(x => x.UserId).FirstOrDefault());
-                Doctor doc = context.Doctors.FirstOrDefault(x => x.Id ==id);
-                DoctorViewModel obj = new DoctorViewModel();
-                obj.Address = doc.Address;
-                obj.BirthDate = doc.BirthDate;
-                obj.Degree = doc.Degree;
-                obj.Gender = doc.Gender;
-                obj.Name = doc.Name;
-                obj.Email = user.Email;
-                obj.Phone = doc.Phone;
-                obj.SSN = doc.SSN;
-                obj.WorkStartTime = doc.WorkStartTime;
-                obj.Photo = doc.Photo;
-                obj.Id = doc.Id;
-                return obj;
+            Doctor doc = context.Doctors.FirstOrDefault(x => x.Id == id);
+            DoctorViewModel obj = new DoctorViewModel();
+            obj.Address = doc.Address;
+            obj.BirthDate = doc.BirthDate;
+            obj.Degree = doc.Degree;
+            obj.Gender = doc.Gender;
+            obj.Name = doc.Name;
+            obj.Email = user.Email;
+            obj.Phone = doc.Phone;
+            obj.SSN = doc.SSN;
+            obj.WorkStartTime = doc.WorkStartTime;
+            obj.Photo = doc.Photo;
+            obj.Id = doc.Id;
+            return obj;
         }
 
-      
+
         public IEnumerable<DoctorViewModel> GetAll(int id, int ShiftId)
         {
             List<DoctorViewModel> doc = new List<DoctorViewModel>();
@@ -217,8 +223,8 @@ namespace BLL.Services
 
         public bool SSNUnUsed(string ssn)
         {
-            var Ssn=context.Doctors.Where(x=>x.SSN==ssn).FirstOrDefault();
-            if (Ssn!=null)
+            var Ssn = context.Doctors.Where(x => x.SSN == ssn).FirstOrDefault();
+            if (Ssn != null)
             {
                 return false;
             }
