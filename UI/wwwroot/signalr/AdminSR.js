415ed4d5187e16cf9b7abd43a0d194e94b09bae2
mohamed121helmy@gmail.com <mohamed121helmy@gmail.com>
2022-05-09 13:39:35 +0200

    aaaa

--- UI/wwwroot/signalr/AdminSR.js
+++ UI/wwwroot/signalr/AdminSR.js
@@ -1,6 +1,23 @@
-﻿var connection = new signalR.HubConnectionBuilder().withUrl("/Realtime").build();
+﻿var x = document.getElementById("myAudio");
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/Realtime").build();
 connection.on("GetNewSergery", function () {
-    location.reload();
+    
+
+    var counter1 = parseInt($('#notificationCounter').text());
+    alert(counter1);
+    counter1 = counter1 + 1;
+    $('#notificationCounter').text(counter1);
+    
+    $('#alertInfo').prepend(`<li class="nav-item">
+                                            <div class="text-center">
+                                                <a href="/SurgeryDoctor/WaitingPage">
+                                                    <strong>ther are an suregery</strong>
+                                                    <i class="fas fa-angle-right"></i>
+                                                </a>
+                                            </div>
+                                        </li>`);
+    x.play();
 });
 
 connection.start();
