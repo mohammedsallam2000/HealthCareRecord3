c9019eb604f8fc9e061b2d8824bb1ade2883b62e
mohammedsallam <mohammedsallam812@gmail.com>
2022-05-23 01:04:50 +0200

    Sallam

--- DAL/Entities/PatientMedicine.cs
+++ DAL/Entities/PatientMedicine.cs
@@ -11,10 +11,8 @@ namespace DAL.Entities
     {
         public int Id { get; set; }
         public bool State { get; set; }
-        public string DateAndTime { get; set; }
         public string Note { get; set; }
         public bool Cancel { get; set; }
-        public Doctor Doctor { get; set; }
         public int? MedicineId { get; set; }
         [ForeignKey("MedicineId")]
         public Medicine Medicine { get; set; }
