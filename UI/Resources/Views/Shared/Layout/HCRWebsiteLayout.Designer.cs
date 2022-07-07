cd0b665c113bb4c0e932535adde9df1fab62ad14
mohammedsallam <mohammedsallam812@gmail.com>
2022-07-07 21:45:22 +0200

    Sallam

--- UI/Resources/Views/Shared/Layout/HCRWebsiteLayout.Designer.cs
+++ UI/Resources/Views/Shared/Layout/HCRWebsiteLayout.Designer.cs
@@ -19,7 +19,7 @@ namespace UI.Resources.Views.Shared.Layout {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class HCRWebsiteLayout {
@@ -84,6 +84,15 @@ namespace UI.Resources.Views.Shared.Layout {
         public static string Contacts {
             get {
                 return ResourceManager.GetString("Contacts", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Contacts.
+        /// </summary>
+        public static string Contacts2 {
+            get {
+                return ResourceManager.GetString("Contacts2", resourceCulture);
             }
         }
         
