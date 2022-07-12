12f8c051da5dd6458d5d64cb2a8ce9a22a8ca5ef
mohamed121helmy@gmail.com <mohamed121helmy@gmail.com>
2022-07-12 17:22:53 +0200

    a

---
+++ UI/Controllers/ReportController.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Services.PatientServices;
+using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+
+namespace UI.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IPatientServices patient;
+
+        public ReportController(IPatientServices patient)
+        {
+            this.patient = patient;
+        }
+        public IActionResult PatiantProfile(int id)
+        {
+            var data=patient.GetByID(id);
+            return new ViewAsPdf(data);
+        }
+    }
+}
