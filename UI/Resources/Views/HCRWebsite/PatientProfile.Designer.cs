8cd97d48e38db02152588f8e74164fbc18f11252
mohammedsallam <mohammedsallam812@gmail.com>
2022-07-14 17:19:37 +0200

    Sallam

--- UI/Resources/Views/HCRWebsite/PatientProfile.Designer.cs
+++ UI/Resources/Views/HCRWebsite/PatientProfile.Designer.cs
@@ -19,7 +19,7 @@ namespace UI.Resources.Views.HCRWebsite {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class PatientProfile {
@@ -57,6 +57,24 @@ namespace UI.Resources.Views.HCRWebsite {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to You must booing an appointement to enjoy Our services.
+        /// </summary>
+        public static string aa {
+            get {
+                return ResourceManager.GetString("aa", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to You are new in the system , so that is no data.
+        /// </summary>
+        public static string aaa {
+            get {
+                return ResourceManager.GetString("aaa", resourceCulture);
             }
         }
         
